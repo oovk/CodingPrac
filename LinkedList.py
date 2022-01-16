6805f1551389e56bfb2b9c794fc7f38f178044c5
oovk <vaibhavrkhandekar@gmail.com>
2022-01-16 18:13:55 +0100

    linkedlist

---
+++ LinkedList.py
@@ -0,0 +1,78 @@
+#Linked List: The elements are not stored continously. They are connected via pointers. It has dynamic size and easy insertion and deletion.
+
+class Node:
+ 
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # Initialize next as null
+ 
+ 
+# Linked List class contains a Node object
+class LinkedList:
+ 
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+ 
+    # This function prints contents of linked list
+    # starting from head
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print (temp.data)
+            temp = temp.next
+    
+    #This function adds new node to the front of linked list
+    def push(self,new_data):
+        new_node = Node(new_data)
+
+        new_node.next = self.head
+
+        self.head = new_node
+
+    #This function inserts a node after a certain node
+    def insertAfter(self, prev_node, new_data):
+
+        if prev_node is None:
+            print("the given node must be available in linked list")
+            return
+        else:
+            new_node = Node(new_data) #creating a node with new data
+            new_node.next = prev_node.next #make new node as next of prev node
+            prev_node.next = new_node #make next of prev node as new node
+
+    #This function adds the node at the end of linked List
+    def append(self,new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            last = self.head
+            while(last.next):
+                last = last.next
+            last.next = new_node
+
+# Code execution starts here
+if __name__=='__main__':
+ 
+    # Start with the empty list
+    llist = LinkedList()
+ 
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+ 
+    llist.head.next = second; # Link first node with second
+    second.next = third; # Link second node with the third node
+ 
+    llist.append(4)
+    llist.push(5)
+    llist.append(6)
+    llist.push(7)
+    
+    llist.insertAfter(llist.head.next.next, 8)
+
+    llist.printList()
